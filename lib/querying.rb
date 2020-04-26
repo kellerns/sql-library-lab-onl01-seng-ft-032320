4dc7fb3523e95bb521a1e1693a038f13fc5a66a7
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 19:56:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,17 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+	FROM series
+	JOIN books
+	ON series.id = books.series_id
+	JOIN character_books
+	ON character_books.id = books.id
+	JOIN characters
+	ON characters.id = character_books.character_id
+	WHERE characters.species = 'human'
+	GROUP BY species
+	ORDER BY COUNT(*);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
