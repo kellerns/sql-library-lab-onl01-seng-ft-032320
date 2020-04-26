f64b7874861ddf2de52b324268d1ae5da9bbea16
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 20:09:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ def select_series_title_with_most_human_characters
 	ON characters.id = character_books.character_id
 	WHERE characters.species = 'human'
 	GROUP BY species
-	ORDER BY COUNT(*);"
+	ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
